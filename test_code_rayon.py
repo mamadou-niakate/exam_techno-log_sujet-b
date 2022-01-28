15714adf45ac9333f9da5d9a482e8e5503f587cd
niakate <mamadouniakate382@gmail.com>
2022-01-28 13:57:10 +0100

    perfomed unit test against jeu validation

--- test_code_rayon.py
+++ test_code_rayon.py
@@ -4,4 +4,4 @@ from CodeRayon import CodeRayon
 def test_code_rayon():
     code_rayon = CodeRayon("FRA.12.42.2021")
 
-    assert code_rayon.is_code_rayon_valide()
+    assert code_rayon.is_code_rayon_valide() == True
