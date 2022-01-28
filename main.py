99d84bc2bfe0f00f65f37ecc8e2d1d566eb832e5
niakate <mamadouniakate382@gmail.com>
2022-01-28 13:48:11 +0100

    definition classe CodeRayon

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+from Jeu import Jeu
+from CodeRayon import  CodeRayon
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    code_rayon = CodeRayon("FRA.12.42.2021")
+    jeu = Jeu("console", "sport", 200, "cool description", code_rayon)
+
+    print(jeu.is_jeu_valide())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
